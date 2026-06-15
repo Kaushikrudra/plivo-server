20d0f10e5960001459d96203932bb3d1a985849d
Kaushik Rudra <86282913+Kaushikrudra@users.noreply.github.com>
2026-06-16 04:29:27 +0530

    Change number column type to VARCHAR(255)

--- server/server.js
+++ server/server.js
@@ -46,7 +46,7 @@ async function initDB() {
         name VARCHAR(255) NOT NULL,
         username VARCHAR(255) UNIQUE NOT NULL,
         password VARCHAR(255) NOT NULL,
-        number VARCHAR(50) DEFAULT '',
+        number VARCHAR(255) DEFAULT '',
         role VARCHAR(50) DEFAULT 'agent',
         created_at TIMESTAMP DEFAULT NOW()
       );
